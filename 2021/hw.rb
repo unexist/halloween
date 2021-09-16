ca290922271edb354b6abcbad7947bc547368074
Christoph Kappel <christoph@unexist.dev>
2021-09-16 10:12:35 +0200

    Added page for 2021

---
+++ 2021/hw.rb
@@ -0,0 +1,48 @@
+require "rubygems"
+require "sinatra"
+require "haml"
+require "data_mapper"
+require "json"
+
+# DatamMpper
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/hw.db")
+
+# Config
+configure do
+  set :port, 8000
+end
+
+class Ghost
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :name,       String
+  property :tested,     Boolean
+  property :vacced,     Boolean
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+Ghost.auto_upgrade!
+
+# Config
+configure do
+  set :port, 8000
+end
+
+# Routes
+get "/" do
+  @ghosts = Ghost.all(:order => [ :created_at.desc ])
+
+  haml :index
+end
+
+post "/ghost" do
+  Ghost.create(name: params["name"],
+    tested: params["tested"],
+    vacced: params["vacced"],
+    created_at: Time.now
+  )
+
+  200
+end
---
+++ 2021/public/js/hw.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+  /**
+   * Show toast message
+   *
+   * @param {String}  str  Message string to show
+   ***/
+
+  function showToast(str)
+  {
+    /* Insert flash element */
+    var flash = $('<div class="flash" style="display: none;"></div>');
+    var box   = $('<div class="box primary"></div>');
+    var mesg  = $('<span></span>');
+
+    mesg.append(str);
+    box.append(mesg);
+    flash.append(box);
+
+    /* Click to hide box immediately */
+    flash.click(function()
+      {
+        flash.remove();
+      }
+    );
+
+    /* Find count of flash elements */
+    var flashes = $("div.flash");
+
+    $("#page").append(flash);
+
+    if(0 < flashes.length)
+      {
+        flash.css("top", (parseInt(flash.css("top")) -
+          flashes.length * (flash.outerHeight(true) + 10)) + "px");
+      }
+
+    /* Display message for some time */
+    flash.fadeIn("slow").delay(5000).hide("slow", function()
+      {
+        var height = flash.outerHeight(true);
+
+        flash.remove();
+
+        var flashes = $("div.flash");
+
+        if(0 < flashes.length)
+          {
+            for(var i = 0; i < flashes.length; i++)
+              {
+                var f = $(flashes[i]);
+
+                f.css("top", (parseInt(f.css("top")) -
+                  - height + 10) + "px");
+              }
+          }
+      }
+    );
+  }
+
+  /* Handle submit */
+  $("#submit_vac, #submit_tst").click(function(e) {
+    var name = $("#name").val();
+
+    if("" != name)
+    {
+      var isTested = "submit_tst" === e.target.id;
+
+        /* Store data */
+        $.ajax({
+          type: "POST",
+          url: "/ghost",
+          data: {
+            name:   name,
+            tested: isTested,
+            vacced: "submit_vac" === e.target.id
+          },
+
+          /* Success handler */
+          success: function(data, status) {
+            /* Insert data */
+            $("#list").append('<div class="' + (isTested ? "tested" : '') + '">' + name + '</div>');
+
+            /* Clear */
+            $("#name").val("");
+
+            /* Update UI */
+            showToast("Yay");
+          },
+
+          /* Error handler */
+          error: function(xhr, status, e) {
+            showToast("Something went wrong: " + status);
+          }
+        });
+
+      }
+    else showToast("Something is missing!");
+  });
+});
