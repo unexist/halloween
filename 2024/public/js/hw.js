cafc4c28aba09d8a11a746e241c440fbd1df50e7
Christoph Kappel <christoph@unexist.dev>
2024-08-18 18:57:19 +0200

    Removed animation and removed dingbats on reveal

--- 2024/public/js/hw.js
+++ 2024/public/js/hw.js
@@ -60,6 +60,7 @@ $(document).ready(function() {
     /* Handle font */
     $("#font-switch").click(function() {
       $("body").css("font-family", '"Comic Sans MS", "Comic Sans", cursive');
+      $(".dingbats").removeClass("dingbats");
     });
 
     /* Handle submit */
