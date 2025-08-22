7a594be4bc87a4c173c5727b11064f4416b2ac8e
Christoph Kappel <christoph@unexist.dev>
2025-08-22 17:10:23 +0200

    Updated files

--- 2025/src/app.rs
+++ 2025/src/app.rs
@@ -1,12 +1,21 @@
+///
+/// @package halloween
+///
+/// @file App functions
+/// @copyright 2025-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <h1>{ "Welcome to Halloween Land!" }</h1>
         </main>
     }
 }
