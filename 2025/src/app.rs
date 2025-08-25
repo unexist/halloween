804fed4d6a5b2c00c19cd1d06808f53bd94dbdf5
Christoph Kappel <christoph@unexist.dev>
2025-08-25 17:31:23 +0200

    Updated handling drawing pipeline

--- 2025/src/app.rs
+++ 2025/src/app.rs
@@ -9,13 +9,218 @@
 /// See the file LICENSE for details.
 ///
 
-use yew::prelude::*;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <h1>{ "Welcome to Halloween Land!" }</h1>
-        </main>
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{window, HtmlCanvasElement, HtmlImageElement, WebGlFramebuffer, WebGlRenderingContext as GL, WebGlRenderingContext, WebGlTexture};
+use yew::{html, Component, Context, Html, NodeRef};
+
+#[derive(Default)]
+pub struct App {
+    node_ref: NodeRef,
+    width: u32,
+    height: u32,
+}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let win = window().unwrap();
+
+        Self {
+            width: (win.inner_width().unwrap().as_f64().unwrap() * win.device_pixel_ratio()) as u32,
+            height: (win.inner_height().unwrap().as_f64().unwrap() * win.device_pixel_ratio()) as u32,
+            ..Default::default()
+        }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <canvas ref={self.node_ref.clone()} />
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
+        // Only start the render loop if it's the first render
+        // There's no loop cancellation taking place, so if multiple renders happen,
+        // there would be multiple loops running. That doesn't *really* matter here because
+        // there's no props update and no SSR is taking place, but it is something to keep in
+        // consideration
+        if !first_render {
+            return;
+        }
+
+        // Create canvas
+        let canvas = self.node_ref.cast::<HtmlCanvasElement>().unwrap();
+
+        canvas.set_width(self.width);
+        canvas.set_height(self.height);
+
+        let gl: GL = canvas
+            .get_context("webgl")
+            .unwrap()
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+
+        self.render_gl(gl);
+    }
+}
+
+impl App {
+    fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+        window()
+            .unwrap()
+            .request_animation_frame(f.as_ref().unchecked_ref())
+            .expect("should register `requestAnimationFrame` OK");
+    }
+
+    fn create_texture(gl: &WebGlRenderingContext) -> WebGlTexture {
+        let texture = gl.create_texture().unwrap();
+
+        gl.bind_texture(GL::TEXTURE_2D, Some(&texture));
+
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::LINEAR as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::LINEAR as i32);
+
+        texture
+    }
+
+    fn init_texture(gl: &WebGlRenderingContext, texture: &WebGlTexture, width: u32, height: u32) {
+        gl.bind_texture(GL::TEXTURE_2D, Some(texture));
+        gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view(GL::TEXTURE_2D, 0, GL::RGBA as i32,
+                                                                                               width as i32, height as i32, 0, GL::RGBA,
+                                                                                               GL::UNSIGNED_BYTE, None)
+            .expect("Failed to create texture");
+    }
+
+    fn create_framebuffer(gl: &WebGlRenderingContext, texture: &WebGlTexture) -> WebGlFramebuffer {
+        let framebuffer = gl.create_framebuffer().unwrap();
+
+        gl.bind_framebuffer(GL::FRAMEBUFFER, Some(&framebuffer));
+        gl.framebuffer_texture_2d(GL::FRAMEBUFFER, GL::COLOR_ATTACHMENT0, GL::TEXTURE_2D, Some(texture), 0);
+
+        framebuffer
+    }
+
+    fn render_gl(&self, gl: WebGlRenderingContext) {
+        let vert_code = include_str!("./hw.vert");
+        let frag_code = include_str!("./hw.frag");
+
+        // Create a buffer for the swaures's positions
+        let vertices: Vec<f32> = vec![
+            -1.0, -1.0,
+            1.0, -1.0,
+            -1.0, 1.0,
+            -1.0, 1.0,
+            1.0, -1.0,
+            1.0, 1.0,
+        ];
+        let vertex_buffer = gl.create_buffer().unwrap();
+        let verts = js_sys::Float32Array::from(vertices.as_slice());
+
+        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&vertex_buffer));
+        gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &verts, GL::STATIC_DRAW);
+
+        // Compile vert shader
+        let vert_shader = gl.create_shader(GL::VERTEX_SHADER).unwrap();
+
+        gl.shader_source(&vert_shader, vert_code);
+        gl.compile_shader(&vert_shader);
+
+        // Compile frag shader
+        let frag_shader = gl.create_shader(GL::FRAGMENT_SHADER).unwrap();
+
+        gl.shader_source(&frag_shader, frag_code);
+        gl.compile_shader(&frag_shader);
+
+        // Create shader program
+        let shader_program = gl.create_program().unwrap();
+
+        gl.attach_shader(&shader_program, &vert_shader);
+        gl.attach_shader(&shader_program, &frag_shader);
+        gl.link_program(&shader_program);
+
+        gl.use_program(Some(&shader_program));
+
+        // Set up position attribute
+        let position = gl.get_attrib_location(&shader_program, "a_position") as u32;
+
+        gl.vertex_attrib_pointer_with_i32(position, 2, GL::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(position);
+
+        // Create textures and framebuffers for green and red shaders
+        let textureGreen = Self::create_texture(&gl);
+
+        Self::init_texture(&gl, &textureGreen, self.width, self.height);
+
+        let framebufferGreen = Self::create_framebuffer(&gl, &textureGreen);
+
+        let textureRed = Self::create_texture(&gl);
+
+        Self::init_texture(&gl, &textureRed, self.width, self.height);
+
+        let framebufferRed = Self::create_framebuffer(&gl, &textureRed);
+
+        // Load and set up image texture
+        let texImage = Self::create_texture(&gl);
+
+        let image = HtmlImageElement::new().unwrap();
+
+        image.set_src("images/circle.jpg");
+
+        // Gloo-render's request_animation_frame has this extra closure
+        // wrapping logic running every frame, unnecessary cost.
+        // Here constructing the wrapped closure just once.
+
+        let cb = Rc::new(RefCell::new(None));
+
+        *cb.borrow_mut() = Some(Closure::wrap(Box::new({
+            let cb = cb.clone();
+            let width = self.width;
+            let height = self.height;
+
+            let performance = web_sys::window().unwrap()
+                .performance().expect("Performance should be available");
+
+            move || {
+                // This should repeat every frame
+                gl.bind_texture(GL::TEXTURE_2D, Some(&texImage));
+                gl.tex_image_2d_with_u32_and_u32_and_image(GL::TEXTURE_2D, 0, GL::RGBA as i32, GL::RGBA,
+                                                           GL::UNSIGNED_BYTE, &image).expect("Failed to set image");
+
+                gl.viewport(0, 0, width as i32, height as i32);
+
+                gl.use_program(Some(&shader_program));
+                gl.bind_framebuffer(GL::FRAMEBUFFER, None);
+                gl.uniform2f(Some(&gl.get_uniform_location(&shader_program, "u_resolution").unwrap()), width as f32, height as f32);
+                gl.uniform2fv_with_f32_array(Some(&gl.get_uniform_location(&shader_program, "u_imageSize").unwrap()), &[1794 as f32, 1024 as f32]);
+
+                let current_time = performance.now() * 0.001;
+
+                gl.uniform1f(Some(&gl.get_uniform_location(&shader_program, "u_time").unwrap()), current_time as f32);
+
+                //let aspect_ratio = (width / height) as f32;
+
+                //gl.uniform1f(Some(&gl.get_uniform_location(&shader_program, "u_aspectRatio").unwrap()), aspect_ratio);
+
+                gl.uniform1i(Some(&gl.get_uniform_location(&shader_program, "u_isMobile").unwrap()), 0);
+
+                gl.active_texture(GL::TEXTURE1);
+                gl.bind_texture(GL::TEXTURE_2D, Some(&texImage));
+                gl.uniform1i(Some(&gl.get_uniform_location(&shader_program, "iChannel0").unwrap()), 0);
+                gl.draw_arrays(GL::TRIANGLES, 0, 6);
+
+                App::request_animation_frame(cb.borrow().as_ref().unwrap());
+            }
+        }) as Box<dyn FnMut()>));
+
+        App::request_animation_frame(cb.borrow().as_ref().unwrap());
     }
 }
